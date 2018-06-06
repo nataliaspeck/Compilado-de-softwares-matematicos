d481bf80b3c76648c385b8e47a009528c60ef33f
Natália Speck <39752995+nataliaspeck@users.noreply.github.com>
2018-06-06 14:16:15 -0300

    Update Naturais.c

--- Naturais.c
+++ Naturais.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 #include <locale.h>
 
-/* Faça um algoritmo para calcular a somatória, a soma dos quadrados e a média dos n
-primeiros números naturais.   */
+/* FaÃ§a um algoritmo para calcular a somatÃ³ria, a soma dos quadrados e a mÃ©dia dos n
+primeiros nÃºmeros naturais.   */
 
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL, "Portuguese");
 	int num, soma = 0, soma2 = 0, media, cont, x;
-	printf ("Insira a quantidade de números inteiros positivos desejada:\n");
+	printf ("Insira a quantidade de nÃºmeros inteiros positivos desejada:\n");
 	scanf ("%d", &num);
 	x = num;
 	for (cont = 1; cont <= x; cont++, num--){
 		soma = soma + num;
 		soma2 = soma2 + (num*num);
 	}
-	printf ("A somatória dos %d primeiros números naturais é: %d\n", x, soma);
-	printf ("A somatória dos quadrados dos %d primeiros números naturais é: %d\n", x, soma2);
-	printf ("A média dos %d primeiros números naturais é de: %3.2f\n", x, (float)soma/num);
+	printf ("A somatÃ³ria dos %d primeiros nÃºmeros naturais Ã©: %d\n", x, soma);
+	printf ("A somatÃ³ria dos quadrados dos %d primeiros nÃºmeros naturais Ã©: %d\n", x, soma2);
+	printf ("A mÃ©dia dos %d primeiros nÃºmeros naturais Ã© de: %3.2f\n", x, (float)soma/x);
 	system ("pause");
 	return 0;
 }
